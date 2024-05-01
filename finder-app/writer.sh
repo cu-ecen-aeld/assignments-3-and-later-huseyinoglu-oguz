fc1781bbb31311ebb5bb9ab1d2b322f90849ecdc
oguz huseyinoglu <oguz.huseyinoglu@gmail.com>
2024-05-01 20:30:22 +0300

    first assignment implementation

---
+++ finder-app/writer.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]; then
+        echo "Error: Argument missing!"
+        exit 1
+fi
+
+
+for file_path_info in "$1"; do
+	mkdir -p -- "$(dirname -- "$file_path_info")"
+	touch -- "$file_path_info"
+done
+
+echo "$2" >> "$1"
+
