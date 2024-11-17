c2479faa4ffe35042896f9e294eb41d306355531
Oğuz Hüseyinoğlu <oguz.huseyinoglu@gmail.com>
2024-11-17 20:27:00 +0300

    implementation of system call

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -16,6 +16,13 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
+    int returnCode = system(cmd);
+
+
+    if(returnCode != 0)
+    {
+        return false;
+    }
 
     return true;
 }
@@ -58,6 +65,33 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
+    fflush(stdout);
+    int status;
+    pid_t pid = fork();
+    
+
+    if(pid < 0)
+    {
+        perror("Failed to fork process");
+        exit(1);
+    }
+    else if(pid == 0)
+    {
+        int returnCode = execv(command[0],command);
+
+        if(returnCode == -1)
+        {
+            perror("execv");
+            exit(EXIT_FAILURE);
+        }
+
+    }
+    else{
+        waitpid(pid,&status,0);
+        if(status != 0)
+            return false;
+    }
+
 
     va_end(args);
 
@@ -92,8 +126,48 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
+    /*
+    int fd = open(outputfile,O_CREAT | O_APPEND|O_WRONLY, 00700);
+    dup2(fd,1);
+
+    int returnCode = execv(command[0],command);
+    close(fd);
+
+    if(returnCode == -1)
+        return false;
+ */
+
+    int fd = open(outputfile,O_CREAT | O_TRUNC|O_WRONLY, 00700);
+	if(fd < 0)
+	{
+		perror("Unable to open file!\n");
+		exit(1);
+	}
+
+	pid_t pid = fork();
+
+	if(pid < 0)
+		perror("Unable to create fork()!\n");
+	else if(pid == 0)
+	{
+		dup2(fd,1);
+		close(fd);
+		execv(command[0],command);
+		exit(-1);
+
+	}
+	else{
+		close(fd);
+		int status;
+		waitpid(pid,&status,0);
+		if(status < 0)
+			return false;
+	}
+
 
     va_end(args);
+
+    
 
     return true;
 }
--- examples/systemcalls/systemcalls.h
+++ examples/systemcalls/systemcalls.h
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h> 
+#include <stdio.h> 
+#include <fcntl.h> 
+#include <sys/wait.h>
 
 bool do_system(const char *command);
 
